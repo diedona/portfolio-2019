be61e3a5a63048913453c0dd1208ed995b828070
Diego Doná <diedona@gmail.com>
2019-10-18 15:05:36 -0300

    removing dist + bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,9 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'
+        ],
       }
     ]
   },
