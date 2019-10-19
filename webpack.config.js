d8526b8fb8ee580d97442fb5da4530c82e5756c7
Diego Doná <diedona@gmail.com>
2019-10-19 11:46:47 -0300

    new thematic, light buld

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|svg|jpg|bmp|gif)$/,
+        test: /\.(png|svg|jpg|jpeg|bmp|gif)$/,
         loader: 'file-loader',
         options: {
           outputPath: 'images',
