80da2544015a9ef105d897aa92233a1d272c7703
Diego Doná <diedona@gmail.com>
2019-10-19 21:34:20 -0300

    build proccess

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,19 @@ const gulpif = require('gulp-if');
 const minify = require('gulp-minify');
 const minifyCss = require('gulp-clean-css');
 const del = require('del');
+const cacheBuster = require('gulp-cache-bust');
+
+const minifyOptions = {
+    ext:{
+        src:'-debug.js',
+        min:'.js'
+    },
+};
 
 function userefTask() {
     return gulp.src('./src/**/*.html')
         .pipe(useref())
-        .pipe(gulpif('*.js', minify()))
+        .pipe(gulpif('*.js', minify(minifyOptions)))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(gulp.dest('dist'));
 }
@@ -32,6 +40,12 @@ function deleteDistTask() {
     return del(['./dist']);
 }
 
+function cacheBustTask() {
+    return gulp.src('./dist/index.html')
+        .pipe(cacheBuster())
+        .pipe(gulp.dest('./dist'));
+}
+
 exports.default = gulp.series(
     deleteDistTask,
     gulp.parallel(
@@ -39,5 +53,6 @@ exports.default = gulp.series(
         copyImagesTask,
         copyFaviconTask,
         copyFontsTask
-    )
+    ),
+    cacheBustTask
 )
