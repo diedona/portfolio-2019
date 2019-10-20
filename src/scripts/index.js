145325cd1d1611b8e7edcf4ee897dd71c6db77db
Diego Doná <diedona@gmail.com>
2019-10-19 21:52:04 -0300

    preload

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,42 @@
 (function () {
 
+    const content = {
+        home: '',
+        objetivos: ''
+    };
+
     $(function () {
 
+        loadAllContent();
+
         const mainContent = $("#mainContent");
-        mainContent.load('./partials/home.html');
+        mainContent.load('./partials/home.html', (html) => {
+            content.home = html;
+        });
 
         $(document).on('click', '#btnObjetivos', function (evt) {
             evt.preventDefault();
-            mainContent.load('./partials/objetivos.html');
+            mainContent.html(content.objetivos);
+            scrollToTop();
         });
 
         $(document).on('click', '.btn-go-home', function (evt) {
             evt.preventDefault();
-            mainContent.load('./partials/home.html');
+            mainContent.html(content.home);
+            scrollToTop();
         });
 
-    });
+    }); // jQuery
+
+    function loadAllContent() {
+        const objetivos = $('<div>');
+        objetivos.load('./partials/objetivos.html', () => {
+            content.objetivos = objetivos.html();
+        });
+    }
+
+    function scrollToTop() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    }
 
 }());
