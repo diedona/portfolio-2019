647a3e3cf1accc7667a60bc3e905293998a90f39
Diego Doná <diedona@gmail.com>
2019-10-21 09:25:14 -0300

    github

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,12 +4,20 @@
         home: '',
         objetivos: '',
         skills: '',
-        contato: ''
+        contato: '',
+        github: ''
+    };
+
+    let githubData = {
+        url: 'https://api.github.com/users/diedona/repos?sort=created&direction=desc&page=1&per_page=10',
+        data: [],
+        html: ''
     };
 
     $(function () {
 
         loadAllContent();
+        loadGithubRepos();
 
         const mainContent = $("#mainContent");
         mainContent.load('./partials/home.html', (html) => {
@@ -34,6 +42,12 @@
             mainContent.html(content.contato);
         });
 
+        $(document).on('click', '#btnGithub', function (evt) {
+            evt.preventDefault();
+            scrollToTop();
+            mainContent.html(generateHtmlForGithub());
+        });
+
         $(document).on('click', '.btn-go-home', function (evt) {
             evt.preventDefault();
             scrollToTop();
@@ -44,6 +58,12 @@
 
     function scrollToTop() {
         $("html, body").animate({ scrollTop: 0 }, "fast");
+    }
+
+    function loadGithubRepos() {
+        $.getJSON(githubData.url, (data) => {
+            githubData.data = data;
+        })
     }
 
     function loadAllContent() {
@@ -61,6 +81,40 @@
         contato.load('./partials/contato.html', () => {
             content.contato = contato.html();
         });
+
+        const github = $('<div>');
+        github.load('./partials/github.html', () => {
+            content.github = github.html();
+        });
+    }
+
+    function generateHtmlForGithub() {
+        const page = $(content.github);
+        console.log(page.html());
+        let html = '';
+
+        $.each(githubData.data, (idx, repo) => {
+            html += `
+                <div class='col-md-4 mb-3'>
+                    <div class="card card-repo shadow-sm">
+                        <div class="card-body">
+                            <strong>${repo.name}</strong> 
+                            <br />
+                            Criado em ${getBrDate(repo.created_at)}
+                            <br />
+                            <a href="${repo.html_url}" target="_blank">Ir para página</a>
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+
+        $('#gitHubRepos', page).html(html);
+        return page.html();
+    }
+
+    function getBrDate(iso) {
+        return moment(iso).format("DD/MM/YYYY HH:mm");
     }
 
 }());
